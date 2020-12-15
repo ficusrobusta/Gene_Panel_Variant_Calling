f5a34b9c2567c0f103b72ae6c1ab26eaaf022b96
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2020-12-15 14:11:43 +0000

    Update make_sample_list.sh

--- make_sample_list.sh
+++ make_sample_list.sh
@@ -4,5 +4,7 @@ FASTQ_DIR=/data2/Blizard-BoneMarrowFailure/fastq/gene_panel/
 for s in ${FASTQ_DIR}/${PROJECT}-*_S*_L*_R*_*.fastq.gz
 do
     s=$(basename $s | sed 's/_S.*_L.*_R.*_.*.fastq.gz//')
+# basename strps directory and suffix from filename
+# sed replaces _S.*_L.*_R.*_.*.fastq.gz/ to empty space
     echo $s ${FASTQ_DIR}/${s}_S*_L*_R1_*.fastq.gz ${FASTQ_DIR}/${s}_S*_L*_R2_*.fastq.gz
 done | sort -u
